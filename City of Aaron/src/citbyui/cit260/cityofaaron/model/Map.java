69b47ff72fe0e13780de0021f94f403c0cf0db6e
Taylor Williams <taylordwilliams90@gmail.com>
2018-10-13 17:45:57 -0600

    Added all of the classes and did all of the getters and setters for the classes.

--- City of Aaron/src/citbyui/cit260/cityofaaron/model/Map.java
+++ City of Aaron/src/citbyui/cit260/cityofaaron/model/Map.java
@@ -6,11 +6,100 @@
 package citbyui.cit260.cityofaaron.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Meroko
  */
 public class Map implements Serializable {
+    
+    private Location locations;
+    private Point currentLocation;
+    private int rows;
+    private int columns;
+
+    public Map() {
+        
+    }
+
+    
+    
+    public Location getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Location locations) {
+        this.locations = locations;
+    }
+
+    public Point getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Point currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.locations);
+        hash = 37 * hash + Objects.hashCode(this.currentLocation);
+        hash = 37 * hash + this.rows;
+        hash = 37 * hash + this.columns;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rows != other.rows) {
+            return false;
+        }
+        if (this.columns != other.columns) {
+            return false;
+        }
+        if (!Objects.equals(this.locations, other.locations)) {
+            return false;
+        }
+        if (!Objects.equals(this.currentLocation, other.currentLocation)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "locations=" + locations + ", currentLocation=" + currentLocation + ", rows=" + rows + ", columns=" + columns + '}';
+    }
+    
+    
+    
     
 }
